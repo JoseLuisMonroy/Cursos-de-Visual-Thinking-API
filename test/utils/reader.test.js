df34c77b21e2509c942374b97a9b08b7ab87f26a
Jose Monroy <mnry7999@gmail.com>
2022-07-16 13:38:54 -0500

    Lectura y obtención de datos de la base

---
+++ test/utils/reader.test.js
@@ -0,0 +1,9 @@
+const reader = require("./../../lib/utils/reader");
+
+describe("Reader test suite", () => {
+    test('Case 1 Read json file', () => {
+        const test1 = reader.readJsonFile("visualPartners.json");
+
+        expect(test1).not.toBeNull();
+    });
+});
